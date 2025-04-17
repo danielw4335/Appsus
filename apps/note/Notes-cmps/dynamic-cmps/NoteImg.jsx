e22ad71841601ebbc2b05f5716b58980acb1f11f
Ido Feffer <idoppr1@gmail.com>
2025-04-17 15:53:54 +0300

    f/TNoteTodos-first edit

--- apps/note/Notes-cmps/dynamic-cmps/NoteImg.jsx
+++ apps/note/Notes-cmps/dynamic-cmps/NoteImg.jsx
@@ -1,10 +1,8 @@
-export function NoteImg({info: {url, title}}){
-
-    return (
-        <section className="note-img">
-
-        <img src={url} alt={title}  />
-        {title && <p>{title}</p>}
-        </section>
-    )
+export function NoteImg({ info: { url, title } }) {
+  return (
+    <section className="note-img">
+      <img src={url} alt={title} />
+      {title && <p>{title}</p>}
+    </section>
+  )
 }
