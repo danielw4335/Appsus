ef0eeec7c55ff27e885c8224160c01cec3cfd71c
Ido Feffer <idoppr1@gmail.com>
2025-04-17 16:36:44 +0300

    f/NoteTodos-mark todos

--- apps/note/Notes-cmps/dynamic-cmps/NoteTodos.jsx
+++ apps/note/Notes-cmps/dynamic-cmps/NoteTodos.jsx
@@ -1,20 +1,27 @@
-const {useState} = React 
+const { useState } = React
 
-export function NoteTodos({info: {todos}}){
-    const [currTodos, setCurrTodos] = useState(todos)
+export function NoteTodos({ info: { todos } }) {
+  const [currTodos, setCurrTodos] = useState(todos)
 
-    function onToggleTodo(idx){
-        
-    }
+  function onToggleTodo(idx) {
+    const updatedTodos = currTodos.map((todo, i) => {
+      if (i !== idx) return todo
+      if (i === idx)
+        return {
+          ...todo,
+          doneAt: todo.doneAt ? null : Date.now(),
+        }
+    })
+    setCurrTodos(updatedTodos)
+  }
 
-
-    return (
-        <section className="note-todo">
-            {todos.map((todo, idx) => (
-                
-                <p key={idx} onClick={() => onToggleTodo(idx)}>{todo.txt}</p>
-
-            ))}
-        </section>
-    )
+  return (
+    <section className="note-todo">
+      {currTodos.map((todo, idx) => (
+        <p key={idx} onClick={() => onToggleTodo(idx)} className={todo.doneAt ? 'done' : ''}>
+          {todo.txt}
+        </p>
+      ))}
+    </section>
+  )
 }
